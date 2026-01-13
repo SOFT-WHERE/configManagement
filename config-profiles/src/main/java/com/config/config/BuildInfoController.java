1e8f695929d5f4c3941b627bfd96323cbaee2786
Arsalaan☪︎☪︎☪︎ <arsalaan@Arsalaans-MacBook-Air.local>
2026-01-14 02:56:36 +0530

    initial commit

---
+++ config-profiles/src/main/java/com/config/config/BuildInfoController.java
@@ -0,0 +1,27 @@
+package com.config.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class BuildInfoController {
+
+//    @Value("${build.id:default}")
+//    private String buildId;
+//
+//    @Value("${build.version:default}")
+//    private String buildVersion;
+//
+//    @Value("${build.name:default}")
+//    private String buildName;
+
+    private BuildInfo buildInfo;
+
+    @GetMapping("/build-info")
+    public String getBuildInfo(){
+        return "BuildID:-"+buildInfo.getId()+" | "+"BuildName:-"+buildInfo.getName()+" | "+"BuildVersion:-"+buildInfo.getVersion();
+    }
+}
---
+++ config-profiles/src/main/java/com/config/config/ConfigProfilesApplication.java
@@ -0,0 +1,13 @@
+package com.config.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigProfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigProfilesApplication.class, args);
+	}
+
+}
